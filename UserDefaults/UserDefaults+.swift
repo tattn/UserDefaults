25ce32c472311d29ee3ae9056550e1bf6867f3f2
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2019-04-14 01:49:34 +0900

    Convert to Swift 5

--- UserDefaults/UserDefaults+.swift
+++ UserDefaults/UserDefaults+.swift
@@ -11,7 +11,7 @@ import Foundation
 // MARK:- UserDefaults.Key
 
 public extension UserDefaults {
-    public struct Key: RawRepresentable {
+    struct Key: RawRepresentable {
         public private(set) var rawValue: String
 
         public init(_ rawValue: String) {
@@ -27,56 +27,56 @@ public extension UserDefaults {
 // MARK:- getter
 
 public extension UserDefaults {
-    public func get<T: Codable>(_ type: T.Type = T.self, for key: Key) -> T? {
+    func get<T: Codable>(_ type: T.Type = T.self, for key: Key) -> T? {
         guard let data = data(for: key) else { return nil }
         return try? JSONDecoder().decode(T.self, from: data)
     }
 
-    public func object<T>(_ type: T.Type = T.self, for key: Key) -> T? {
+    func object<T>(_ type: T.Type = T.self, for key: Key) -> T? {
         return object(forKey: key.rawValue) as? T
     }
     
-    public func object(for key: Key) -> Any? {
+    func object(for key: Key) -> Any? {
         return object(forKey: key.rawValue)
     }
 
-    public func string(for key: Key) -> String? {
+    func string(for key: Key) -> String? {
         return string(forKey: key.rawValue)
     }
     
-    public func array<T>(_ elementType: T, for key: Key) -> [T]? {
+    func array<T>(_ elementType: T, for key: Key) -> [T]? {
         return array(forKey: key.rawValue) as? [T]
     }
 
-    public func array(for key: Key) -> [Any]? {
+    func array(for key: Key) -> [Any]? {
         return array(forKey: key.rawValue)
     }
     
-    public func dictionary(for key: Key) -> [String : Any]? {
+    func dictionary(for key: Key) -> [String : Any]? {
         return dictionary(forKey: key.rawValue)
     }
 
-    public func data(for key: Key) -> Data? {
+    func data(for key: Key) -> Data? {
         return data(forKey: key.rawValue)
     }
     
-    public func url(for key: Key) -> URL? {
+    func url(for key: Key) -> URL? {
         return url(forKey: key.rawValue)
     }
 
-    public func integer(for key: Key) -> Int {
+    func integer(for key: Key) -> Int {
         return integer(forKey: key.rawValue)
     }
 
-    public func float(for key: Key) -> Float {
+    func float(for key: Key) -> Float {
         return float(forKey: key.rawValue)
     }
 
-    public func double(for key: Key) -> Double {
+    func double(for key: Key) -> Double {
         return double(forKey: key.rawValue)
     }
 
-    public func bool(for key: Key) -> Bool {
+    func bool(for key: Key) -> Bool {
         return bool(forKey: key.rawValue)
     }
 }
@@ -84,32 +84,32 @@ public extension UserDefaults {
 // MARK:- setter
 
 public extension UserDefaults {
-    public func set<T: Codable>(_ value: T, for key: Key) {
+    func set<T: Codable>(_ value: T, for key: Key) {
         guard let data = try? JSONEncoder().encode(value) else { return }
         set(data, forKey: key.rawValue)
     }
 
-    public func set(_ value: Any?, for key: Key) {
+    func set(_ value: Any?, for key: Key) {
         set(value, forKey: key.rawValue)
     }
     
-    public func set(_ url: URL?, for key: Key) {
+    func set(_ url: URL?, for key: Key) {
         set(url, forKey: key.rawValue)
     }
     
-    public func set(_ value: Int, for key: Key) {
+    func set(_ value: Int, for key: Key) {
         set(value, forKey: key.rawValue)
     }
 
-    public func set(_ value: Float, for key: Key) {
+    func set(_ value: Float, for key: Key) {
         set(value, forKey: key.rawValue)
     }
 
-    public func set(_ value: Double, for key: Key) {
+    func set(_ value: Double, for key: Key) {
         set(value, forKey: key.rawValue)
     }
 
-    public func set(_ value: Bool, for key: Key) {
+    func set(_ value: Bool, for key: Key) {
         set(value, forKey: key.rawValue)
     }
 }
@@ -117,7 +117,7 @@ public extension UserDefaults {
 // MARK:- other
 
 public extension UserDefaults {
-    public func remove(for key: Key) {
+    func remove(for key: Key) {
         removeObject(forKey: key.rawValue)
     }
 }
